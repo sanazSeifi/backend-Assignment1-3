715f705a77ebbf90bf95819e061f468177c26963
Sanaz <snz5seifi@gmail.com>
2022-08-22 15:42:03 -0400

    Creat maven project with Spring Initializer

---
+++ src/main/java/com/example/backendAssignment13/BackendAssignment13Application.java
@@ -0,0 +1,13 @@
+package com.example.backendAssignment13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAssignment13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAssignment13Application.class, args);
+	}
+
+}
